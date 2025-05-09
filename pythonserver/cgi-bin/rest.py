b10e104df8aed8480c4436d8b6b5e7b91770bc38
Victor <lilvictor691011@gmail.com>
2025-05-09 11:17:15 +0200

    CORS exercise

---
+++ pythonserver/cgi-bin/rest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import json
+
+print("Access-Control-Allow-Origin: *")
+print("Access-Control-Allow-Methods: POST, GET, OPTIONS")
+print("Content-Type: text/json\n")
+
+print('''
+{"variable":"valor"}
+''')
